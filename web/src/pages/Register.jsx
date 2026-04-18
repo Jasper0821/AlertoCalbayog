51129a89d24da5a623d37b7c168d6ced2dca026a
jaycomendador <178397541+jaycomendador@users.noreply.github.com>
2026-04-18 11:39:14 +0800

    adi

--- web/src/pages/Register.jsx
+++ web/src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-
+//hh
 function Register() {
   const [isAgreementAccepted, setIsAgreementAccepted] = useState(false);
 
